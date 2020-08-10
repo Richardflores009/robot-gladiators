6847eb65c548f8d93ddaedcbf127b7ea26601263
Richy Rich <richardflores009@gmail.com>
2020-08-10 00:31:01 -0700

    added functions and var for enemy and plater

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This is a string leaving us a message");
+console.log(10 + 10);
+console.log("you are a" + playerName)
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
