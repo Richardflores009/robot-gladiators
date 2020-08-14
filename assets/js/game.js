8426ec6f3e4aa5037cd99f76266e13e812fd0975
Richy Rich <richardflores009@gmail.com>
2020-08-13 22:19:09 -0700

    added attack randomness

--- assets/js/game.js
+++ assets/js/game.js
@@ -26,43 +26,56 @@ var fightOrSkip = function () {
     return false;
 };
 var fight = function (enemy) {
+    // keep track of who goes first
+    var isPlayerTurn = true;
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
+    }
     // repeat and execute as long as the enemy robot is alive
     while (enemy.health > 0 && playerInfo.health > 0) {
-        if (fightOrSkip()) {
-            // if true, leave fight by breaking loop
-            break;
-        }
+        if (isPlayerTurn) {
+            if (fightOrSkip()) {
+                // if true, leave fight by breaking loop
+                break;
+            }
 
-        // genrate random damage value based value based on player's attack power
-        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+            // genrate random damage value based value based on player's attack power
+            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
-        enemy.health = Math.max(0, enemy.health - damage);
-        console.log(
-            playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining. "
-        );
+            enemy.health = Math.max(0, enemy.health - damage);
+            console.log(
+                playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining. "
+            );
 
-        // check enemy's health
-        if (enemy.health <= 0) {
-            window.alert(enemy.name + " has died!");
-            break;
+            // check enemy's health
+            if (enemy.health <= 0) {
+                window.alert(enemy.name + " has died!");
+
+                //award player money for winning
+                playerInfo.money = playerInfo.money + 20;
+                break;
+            } else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left ");
+            }
+            // generate random damage value based on player's attack power
         } else {
-            window.alert(enemy.name + " still has " + enemy.health + " health left ");
-        }
-        // generate random damage value based on player's attack power
-        var damage = randomNumber(enemy.attack - 3, enemy.attack);
+            var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
-        playerInfo.health = Math.max(0, playerInfo.health - damage);
-        // Log a resulting message to the console so we know that it worked.
-        console.log(
-            enemy.name + " attacked " + playerInfo.name + " . " + playerInfo.name + " now has " + playerInfo.health + " health remaining "
-        );
-        //check player health
-        if (playerInfo.health <= 0) {
-            window.alert(playerInfo.name + " has died! ");
-            break;
-        } else {
-            window.alert(playerInfo.name + " still has " + playerInfo.health + " health left");
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+            // Log a resulting message to the console so we know that it worked.
+            console.log(
+                enemy.name + " attacked " + playerInfo.name + " . " + playerInfo.name + " now has " + playerInfo.health + " health remaining "
+            );
+            //check player health
+            if (playerInfo.health <= 0) {
+                window.alert(playerInfo.name + " has died! ");
+                break;
+            } else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left");
+            }
         }
+        // swtich turn order for next round
+        isPlayerTurn = !isPlayerTurn;
     }
 };
 // function to generate a random numeric value
@@ -191,7 +204,6 @@ var endGame = function () {
 
 var shop = function () {
     //ask player what they'd like to do
-    debugger;
     var shopOptionPrompt = window.prompt(
         "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
